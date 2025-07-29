ab0b812b0cafc6c320d9fe547a310236b0a4cf0a
Elroy <9432943+E-Andela@users.noreply.github.com>
2025-07-29 14:03:50 +0200

    add structure for project and started on ex00

---
+++ ex00/inc/ScalarConverter.hpp
@@ -0,0 +1,16 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <string>
+
+class ScalarConverter {
+public:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter &other);
+	~ScalarConverter();
+	ScalarConverter &operator=(const ScalarConverter &other);
+
+	static void convert(const std::string &literal);
+};
+
+#endif
